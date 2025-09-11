e6a16c8055c05621b0ae5dc770227de8b8a5530c
EVInfrastructure-co-uk <georgewatson94@hotmail.com>
2025-09-11 15:24:47 +0100

    Update lookupPostcode.js

--- js/lookupPostcode.js
+++ js/lookupPostcode.js
@@ -9,23 +9,12 @@ function lookupPostcode() {
         .then(data => {
           if (data.status === 200 && data.result) {
             const authority = data.result.admin_district; // Local authority name
-            // document.getElementById('result').textContent = `Local Authority: ${authority}`;
-            localStorage.setItem("authority",${authority})
-            window.location.href = `${window.location.origin}/government-and-EVI/local-government.html`
+            document.getElementById('result').textContent = `Local Authority: ${authority}`;
           } else {
             document.getElementById('result').textContent = 'Postcode not found.';
           }
         })
         .catch(() => {
           document.getElementById('result').textContent = 'Error contacting API.';
-        })
-        postcode.addEventListener("submit", (event) => {
-            const searchBarValue = document.getElementById("postcode").value; // get input's value everytime the form is submitted
-            console.log(searchBarValue);
-            event.preventDefault(); // prevent refresh
-            // localStorage.setItem("key", searchBarValue);
-            // const searchKey = localStorage.getItem("key");
-            main(searchBarValue);
         });
-      
     }
