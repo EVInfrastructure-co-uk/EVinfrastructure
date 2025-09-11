6215398f1f85fecc5415d8893c5831d9a53367ef
EVInfrastructure-co-uk <georgewatson94@hotmail.com>
2025-09-11 15:51:06 +0100

    Update lookupPostcodehomepage.js

--- js/lookupPostcodehomepage.js
+++ js/lookupPostcodehomepage.js
@@ -1,4 +1,4 @@
-document.getElementById('postcode__form').addEventListener('submit', (event) {
+document.getElementById('postcode__form').addEventListener('submit', function(event) {
       event.preventDefault();
       const postcode = document.getElementById('postcode').value.trim();
       if (!postcode) {
