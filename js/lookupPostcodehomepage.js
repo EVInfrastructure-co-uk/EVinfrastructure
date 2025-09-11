9000c12d45fcfa78e381a8b5baa0f9197218596a
EVInfrastructure-co-uk <georgewatson94@hotmail.com>
2025-09-11 15:45:11 +0100

    Update lookupPostcodehomepage.js

--- js/lookupPostcodehomepage.js
+++ js/lookupPostcodehomepage.js
@@ -1,14 +1,10 @@
-function lookupPostcodehomepage() {
+document.getElementById('postcode__form').addEventListener('submit', (event) {
+      event.preventDefault();
       const postcode = document.getElementById('postcode').value.trim();
       if (!postcode) {
         document.getElementById('result').textContent = 'Please enter a postcode.';
         return;
       }
-      postcode.addEventListener("submit", (event) => {
-      const searchBarValue = document.getElementById("postcode__form").value;
-      event.preventDefault();
       localStorage.setItem("postcode", postcode);
       window.location.href = `government-and-EVI/local-government.html`;
     });
-     ;
-    }
