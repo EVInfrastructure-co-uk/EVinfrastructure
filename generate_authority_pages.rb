df194ca7f1150f389236eeb4cdbfdb5a82bc3578
EVInfrastructure-co-uk <georgewatson94@hotmail.com>
2025-10-02 10:53:43 +0100

    Add files via upload

--- generate_authority_pages.rb
+++ generate_authority_pages.rb
@@ -1,10 +1,10 @@
 require 'yaml'
 
-data = YAML.load_file('_data/authorities.yml')
-template = File.read('local-government/authority-template.html')
+data = YAML.load_file('_data/authorities.yaml')
+template = File.read('government-and-EVI/local-government/authority-template.html')
 
 data.each do |authority|
   page = template.gsub('{{ authority.name }}', authority['name'])
                  .gsub('{{ authority.cpc_link }}', authority['cpc_link'])
-  File.write("authorities/#{authority['name'].downcase}.html", page)
+  File.write("government-and-EVI/local-government/#{authority['name'].downcase}.html", page)
 end
