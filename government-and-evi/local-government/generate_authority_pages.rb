679a8978f9b634175573cb5caa8f63eb2a4f094c
EVInfrastructure-co-uk <georgewatson94@hotmail.com>
2025-10-12 14:43:01 +0100

    Update generate_authority_pages.rb

--- government-and-evi/local-government/generate_authority_pages.rb
+++ government-and-evi/local-government/generate_authority_pages.rb
@@ -3,16 +3,19 @@ require 'json'
 # Load JSON data from authorities.json
 data = JSON.parse(File.read('data/uk_la_evi.json'))
 authorities = data['resources'][0]['data']
+england_ca = File.read('_templates/england-CA.html')
 england_district = File.read('_templates/england-district.html')
 england_district_ca = File.read('_templates/england-district-CA.html')
 england_county = File.read('_templates/england-county.html')
 england_county_ca = File.read('_templates/england-county-CA.html')
 england_unitary = File.read('_templates/england-unitary.html')
 england_unitary_ca = File.read('_templates/england-unitary-CA.html')
-levi_combined = File.read('_templates/LEVI-combined.html')
-scotland = File.read('_templates/scotland.html')
-wales = File.read('_templates/wales.html')
 ni = File.read('_templates/ni.html')
+scotland_ca = File.read('_templates/scotland-CA.html')
+scotland_unitary_ca = File.read('_templates/scotland-unitary-CA.html')
+wales_ca = File.read('_templates/wales.html')
+wales_unitary_ca = File.read('_templates/wales-unitary-CA.html')
+wales_unitary = File.read('_templates/wales-unitary.html')
 
 # # Create _authorities directory if it doesn't exist
 # Dir.mkdir('_authorities') unless Dir.exist?('_authorities')
@@ -29,13 +32,13 @@ authorities.select { |a| a['local-authority-type'] == 'CC' or a['local-authority
   puts "Generated #{filename}"
 end
 
-authorities.select { |a| a['local-authority-type'] == 'COMB' }.each do |authority|
+authorities.select { |a| a['local-authority-type'] == 'COMB' and a['region'] != 'Wales' and a['region'] != 'Scotland' }.each do |authority|
   name = authority['nice-name']
   slug = authority['gov-uk-slug']
 
   filename = "#{slug}.html"
 
-  content = levi_combined.gsub('{{ authority.name }}',name)
+  content = england_ca.gsub('{{ authority.name }}',name)
 
   File.write(filename, content)
   puts "Generated #{filename}"
@@ -108,30 +111,6 @@ authorities.select { |a| a['local-authority-type'] == 'NMD'}.each do |authority|
 
 end
 
-authorities.select { |a| a['local-authority-type'] == 'SCO' }.each do |authority|
-  name = authority['nice-name']
-  slug = authority['gov-uk-slug']
-
-  filename = "#{slug}.html"
-
-  content = scotland.gsub('{{ authority.name }}',name)
-
-  File.write(filename, content)
-  puts "Generated #{filename}"
-end
-
-authorities.select { |a| a['local-authority-type'] == 'WPA' }.each do |authority|
-  name = authority['nice-name']
-  slug = authority['gov-uk-slug']
-
-  filename = "#{slug}.html"
-
-  content = wales.gsub('{{ authority.name }}',name)
-
-  File.write(filename, content)
-  puts "Generated #{filename}"
-end
-
 authorities.select { |a| a['local-authority-type'] == 'NID' }.each do |authority|
   name = authority['nice-name']
   slug = authority['gov-uk-slug']
@@ -139,6 +118,66 @@ authorities.select { |a| a['local-authority-type'] == 'NID' }.each do |authority
   filename = "#{slug}.html"
 
   content = ni.gsub('{{ authority.name }}',name)
+
+  File.write(filename, content)
+  puts "Generated #{filename}"
+end
+
+authorities.select { |a| a['local-authority-type'] == 'SCO' }.each do |authority|
+  name = authority['nice-name']
+  slug = authority['gov-uk-slug']
+
+  filename = "#{slug}.html"
+
+  content = scotland_unitary_ca.gsub('{{ authority.name }}',name)
+
+  File.write(filename, content)
+  puts "Generated #{filename}"
+end
+
+authorities.select { |a| a['local-authority-type'] == 'COMB' and a['region'] == 'Scotland' }.each do |authority|
+  name = authority['nice-name']
+  slug = authority['gov-uk-slug']
+
+  filename = "#{slug}.html"
+
+  content = scotland_ca.gsub('{{ authority.name }}',name)
+
+  File.write(filename, content)
+  puts "Generated #{filename}"
+end
+
+authorities.select { |a| a['local-authority-type'] == 'COMB' and a['region'] == 'Wales' }.each do |authority|
+  name = authority['nice-name']
+  slug = authority['gov-uk-slug']
+
+  filename = "#{slug}.html"
+
+  content = wales_ca.gsub('{{ authority.name }}',name)
+
+  File.write(filename, content)
+  puts "Generated #{filename}"
+end
+
+authorities.select { |a| a['local-authority-type'] == 'WPA' and a['combined-authority'] == '' }.each do |authority|
+  name = authority['nice-name']
+  slug = authority['gov-uk-slug']
+
+  filename = "#{slug}.html"
+
+  content = wales_unitary.gsub('{{ authority.name }}',name)
+
+  File.write(filename, content)
+  puts "Generated #{filename}"
+end
+
+authorities.select { |a| a['local-authority-type'] == 'WPA' and a['combined-authority'] != '' }.each do |authority|
+  name = authority['nice-name']
+  slug = authority['gov-uk-slug']
+
+  filename = "#{slug}.html"
+
+  content = wales_unitary_ca.gsub('{{ authority.name }}',name)
 
   File.write(filename, content)
   puts "Generated #{filename}"
