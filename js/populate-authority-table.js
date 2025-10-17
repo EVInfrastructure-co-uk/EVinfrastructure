e2d6aa19b21fc9b66dd5ed6687941f653e6b2ba7
EVInfrastructure-co-uk <georgewatson94@hotmail.com>
2025-10-17 10:13:41 +0100

    starting on authority table population

--- js/populate-authority-table.js
+++ js/populate-authority-table.js
@@ -1,1 +1,7 @@
-
+function populate(slug) {
+    fetch('/government-and-EVI/local-government/data/uk_la_evi.json')
+  .then(res => res.json())
+  .then(allData => {
+    // Filter/display data for selected authority
+  });
+};
