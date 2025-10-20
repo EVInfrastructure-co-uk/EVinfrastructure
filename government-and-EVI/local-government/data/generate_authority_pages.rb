d4c8234f5d22d13d20bcbd6637ce1a4ddd744373
EVInfrastructure-co-uk <georgewatson94@hotmail.com>
2025-10-20 15:41:22 +0100

    update code for generating authority pages

--- government-and-EVI/local-government/data/generate_authority_pages.rb
+++ government-and-EVI/local-government/data/generate_authority_pages.rb
@@ -4,18 +4,35 @@ Dir.chdir("..")
 # Load JSON data from authorities.json
 data = JSON.parse(File.read('data/uk_la_evi.json'))
 authorities = data['resources'][0]['data']
+
 england_ca = File.read('_templates/england-CA.html')
+
 england_district = File.read('_templates/england-district.html')
 england_district_ca = File.read('_templates/england-district-CA.html')
+england_district_2ca = File.read('_templates/england-district-2CA.html')
+england_district_3ca = File.read('_templates/england-district-3CA.html')
+england_district_extra_ca = File.read('_templates/england-district-extra-CA.html')
+
 england_county = File.read('_templates/england-county.html')
 england_county_ca = File.read('_templates/england-county-CA.html')
+england_county_2ca = File.read('_templates/england-county-2CA.html')
+england_county_3ca = File.read('_templates/england-county-3CA.html')
+england_county_extra_ca = File.read('_templates/england-county-extra-CA.html')
+
 england_unitary = File.read('_templates/england-unitary.html')
 england_unitary_ca = File.read('_templates/england-unitary-CA.html')
+england_unitary_2ca = File.read('_templates/england-unitary-2CA.html')
+england_unitary_3ca = File.read('_templates/england-unitary-3CA.html')
+england_unitary_extra_ca = File.read('_templates/england-unitary-extra-CA.html')
+england_unitary_extra_2ca = File.read('_templates/england-unitary-extra-2CA.html')
+
 ni = File.read('_templates/ni.html')
+
 scotland_ca = File.read('_templates/scotland-CA.html')
 scotland_unitary_ca = File.read('_templates/scotland-unitary-CA.html')
 scotland_unitary = File.read('_templates/scotland-unitary.html')
 wales_ca = File.read('_templates/wales-CA.html')
+
 wales_unitary_ca = File.read('_templates/wales-unitary-CA.html')
 wales_unitary = File.read('_templates/wales-unitary.html')
 
@@ -28,10 +45,17 @@ authorities.select { |a| a['local-authority-type'] == 'CC' or a['local-authority
 
   filename = "#{slug}.html"
 
-  content = england_unitary_ca.gsub('{{ authority.name }}',name)
-                              .gsub('{{ authority.slug }}',slug)
-  
-  File.write(filename, content)
+  if authority['combined-authority-2'].blank? {
+    content = england_unitary_ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  } elsif authority['combined-authority-3'].blank? {
+    content = england_unitary_2ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  } else {
+    content = england_unitary_3ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  }
+  File.write(filename, content)    
   puts "Generated #{filename}"
 end
 
@@ -67,10 +91,17 @@ authorities.select { |a| a['local-authority-type'] == 'UA' and a['combined-autho
 
   filename = "#{slug}.html"
 
-  content = england_unitary.gsub('{{ authority.name }}',name)
-                              .gsub('{{ authority.slug }}',slug)
-
-  File.write(filename, content)
+  if authority['combined-authority-2'].blank?
+    content = england_unitary.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  elsif authority['combined-authority-3'].blank?
+    content = england_unitary_extra_ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  else
+    content = england_unitary_extra_2ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  end
+  File.write(filename, content)    
   puts "Generated #{filename}"
 end
 
@@ -80,10 +111,17 @@ authorities.select { |a| a['local-authority-type'] == 'UA' and a['combined-autho
 
   filename = "#{slug}.html"
 
-  content = england_unitary_ca.gsub('{{ authority.name }}',name)
-                              .gsub('{{ authority.slug }}',slug)
-
-  File.write(filename, content)
+  if authority['combined-authority-2'].blank?
+    content = england_unitary_ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  elsif authority['combined-authority-3'].blank?
+    content = england_unitary_2ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  else
+    content = england_unitary_3ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug) 
+  end
+  File.write(filename, content)    
   puts "Generated #{filename}"
 end
 
@@ -93,9 +131,13 @@ authorities.select { |a| a['local-authority-type'] == 'CTY' and a['combined-auth
 
   filename = "#{slug}.html"
 
-  content = england_county.gsub('{{ authority.name }}',name)
-                              .gsub('{{ authority.slug }}',slug)
-
+  if authority['combined-authority-2'].blank?
+    content = england_county.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  elsif authority['combined-authority-3'].blank?
+    content = england_county_extra_ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  end
   File.write(filename, content)
   puts "Generated #{filename}"
 end
@@ -106,10 +148,17 @@ authorities.select { |a| a['local-authority-type'] == 'CTY' and a['combined-auth
 
   filename = "#{slug}.html"
 
-  content = england_county_ca.gsub('{{ authority.name }}',name)
-                              .gsub('{{ authority.slug }}',slug)
-
-  File.write(filename, content)
+  if authority['combined-authority-2'].blank?
+    content = england_county_ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  elsif authority['combined-authority-3'].blank? 
+    content = england_county_2ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  else
+    content = england_county_3ca.gsub('{{ authority.name }}',name)
+                                .gsub('{{ authority.slug }}',slug)
+  end
+  File.write(filename, content)    
   puts "Generated #{filename}"
 end
 
@@ -121,12 +170,25 @@ authorities.select { |a| a['local-authority-type'] == 'NMD'}.each do |authority|
 
   authorities.select { |b| b['local-authority-code'] == county_la_code}.each do |county_la|
     county_la_ca =  county_la['combined-authority']
-      if county_la_ca == ''
-        content = england_district.gsub('{{ authority.name }}',name)
-                              .gsub('{{ authority.slug }}',slug)
+      if county_la_ca.blank?
+        if authority['combined-authority-2'].blank?
+          content = england_district.gsub('{{ authority.name }}',name)
+                                      .gsub('{{ authority.slug }}',slug)
+        elsif authority['combined-authority-3'].blank?
+          content = england_district_extra_ca.gsub('{{ authority.name }}',name)
+                                      .gsub('{{ authority.slug }}',slug)
+        end
       else
-        content = england_district_ca.gsub('{{ authority.name }}',name)
-                              .gsub('{{ authority.slug }}',slug)
+        if authority['combined-authority-2'].blank?
+          content = england_district_ca.gsub('{{ authority.name }}',name)
+                                      .gsub('{{ authority.slug }}',slug)
+        elsif authority['combined-authority-3'].blank?
+          content = england_district_2ca.gsub('{{ authority.name }}',name)
+                                      .gsub('{{ authority.slug }}',slug)
+        else
+          content = england_district_3ca.gsub('{{ authority.name }}',name)
+                                      .gsub('{{ authority.slug }}',slug)
+        end
       end
       File.write(filename, content)
       puts "Generated #{filename}"
