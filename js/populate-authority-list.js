f72aeaeeca1c4f141dc6db259c543dee056961e6
EVInfrastructure-co-uk <georgewatson94@hotmail.com>
2025-11-20 16:36:27 +0000

    amend authority list

--- js/populate-authority-list.js
+++ js/populate-authority-list.js
@@ -4,13 +4,28 @@ fetch('/government/local-government/data/uk_la_evi.json')
     laData = jsonData.resources[0].data;
 
     const authority_list = document.getElementById('authority_select');
+    // var authority_array
 
     laData.forEach(authority => {
         if  (authority['local-authority-type'] != "COMB") {
             const option = document.createElement('option');
-            option.value = authority['slug'];
+            option.value = authority['gov-uk-slug'];
             option.textContent = authority['nice-name'];
             authority_list.appendChild(option);
         }
       });
+
+    //   var authority_array = birdList.getElementsByTagName("li");
+
+    // birds = Array.prototype.slice.call(birds).sort(function(a, b) {
+    //     return a.firstChild.data.toLowerCase().localeCompare(b.firstChild.data.toLowerCase());
+    // });
+
+    // for (var i = 0, length = birds.length; i < length; i++) {
+    //                 const option = document.createElement('option');
+    //         option.value = authority['gov-uk-slug'];
+    //         option.textContent = authority['nice-name'];
+    //         authority_list.appendChild(birds[i]);
+    // };
+    
     })
